9328bf1578e89a9bd97731fc2cda490960a5e121
ishakboufatah <boufatah.ishak@gmail.com>
2022-08-11 07:33:10 +0100

    first add

--- EORDatabase/urls.py
+++ EORDatabase/urls.py
@@ -29,4 +29,4 @@ router.register(r'caseStusies',TaskViewSet)
 urlpatterns = [
     path('',include(router.urls)), 
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
