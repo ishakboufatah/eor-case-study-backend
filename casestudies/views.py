f2fdf70b40ba68721a41941a1515bfa89a024b69
ishakboufatah <boufatah.ishak@gmail.com>
2022-08-12 09:29:59 +0100

    ordering searchfilterset

--- casestudies/views.py
+++ casestudies/views.py
@@ -4,13 +4,18 @@
 from .models import CaseStudies,Country
 from .serializers import TaskSerializer,CountrySerializer
 
-from rest_framework import viewsets
+from rest_framework import viewsets,filters
 from rest_framework.permissions import DjangoModelPermissions
+from django_filters.rest_framework import DjangoFilterBackend
 
 class CountryViewSet(viewsets.ModelViewSet):
     permission_classes =[DjangoModelPermissions]
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
+    filter_backends= [DjangoFilterBackend,filters.SearchFilter,filters.OrderingFilter]
+    filterset_fields= ['CaseStudies_id','country','EOR_Type']
+    search_fields= ['CaseStudies_id','country','EOR_Type']
+    ordering_fields= ['CaseStudies_id','country','EOR_Type']
 
 class TaskViewSet(viewsets.ModelViewSet):
     permission_classes =[DjangoModelPermissions]
