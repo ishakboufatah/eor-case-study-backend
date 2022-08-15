bd773b0038eca7ddd4e0880f8a1bcac8c55a3b54
ishakboufatah <boufatah.ishak@gmail.com>
2022-08-15 03:16:48 +0100

    add url params

--- EORDatabase/urls.py
+++ EORDatabase/urls.py
@@ -34,3 +34,19 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+from django.conf.urls import url
+
+from django.conf import settings
+
+from django.views.static import serve
+
+  
+
+urlpatterns += [
+
+  url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+
+  url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+
+]
